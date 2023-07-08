a309864d995a38017d572c8c6f8d0c90df0e6027
mansbaloch <114749425+mansbaloch@users.noreply.github.com>
2023-07-09 01:00:13 +0500

    Create AboutUs.js

---
+++ src/components/AboutUs.js
@@ -0,0 +1,17 @@
+import "./AboutUsStyles.css";
+
+function AboutUs() {
+  return (
+    <div className="about-container">
+      <h1>Our History</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nulla, inventore ducimus nihil perferendis distinctio ipsum, eaque porro in nisi accusamus illo, quae sint rem minima incidunt doloremque suscipit similique!</p>
+      <h1>Our Mission</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nulla, inventore ducimus nihil perferendis distinctio ipsum, eaque porro in nisi accusamus illo.</p>
+    <h1>Our Vision</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nulla, inventore ducimus nihil perferendis distinctio ipsum.</p>
+   
+    </div>
+  );
+}
+
+export default AboutUs;
